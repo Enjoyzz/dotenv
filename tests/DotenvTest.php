3a4a681db011ffd5d2d7df9447cc60597ceb68ac
enjoys <deadly.info@gmail.com>
2021-09-11 01:58:45 +0300

    added tests

--- tests/DotenvTest.php
+++ tests/DotenvTest.php
@@ -4,24 +4,24 @@ declare(strict_types=1);
 
 
 use Enjoys\Dotenv\Dotenv;
+use Enjoys\Dotenv\Exception\InvalidParameter;
 use PHPUnit\Framework\TestCase;
 
 class DotenvTest extends TestCase
 {
 
-    public function testStarted()
+    public function testVariableReplace()
     {
-        $dotenv = new Dotenv(__DIR__.'/fixtures/1');
+        $dotenv = new Dotenv(__DIR__ . '/fixtures/1');
         $dotenv->loadEnv();
 
         $this->assertSame('dev', $_ENV['APP_ENV']);
         $this->assertSame('C:/openserver/test', $_ENV['TEST_DIR']);
-
     }
 
     public function testCastType()
     {
-        $dotenv = new Dotenv(__DIR__.'/fixtures/2');
+        $dotenv = new Dotenv(__DIR__ . '/fixtures/2');
         $dotenv->loadEnv();
 
         $this->assertSame(42, $_ENV['VAR_1']);
@@ -32,15 +32,54 @@ class DotenvTest extends TestCase
         $this->assertSame(3.14, $_ENV['VAR_6']);
         $this->assertSame('3,14', $_ENV['VAR_7']);
         $this->assertSame('', $_ENV['VAR_8']);
-
-
-
     }
 
     public function testEnvWithEq()
     {
-        $dotenv = new Dotenv(__DIR__.'/fixtures/3');
+        $dotenv = new Dotenv(__DIR__ . '/fixtures/3');
         $dotenv->loadEnv();
         $this->assertSame('foo bar = zed', $_ENV['VAR']);
     }
+
+    public function testUsePutEnvTrue()
+    {
+        $dotenv = new Dotenv(__DIR__ . '/fixtures/3');
+        $dotenv->loadEnv(true);
+        $this->assertSame('foo bar = zed', getenv('VAR'));
+
+        //unset
+        putenv('VAR');
+    }
+
+    public function testUsePutEnvFalse()
+    {
+        $dotenv = new Dotenv(__DIR__ . '/fixtures/3');
+        $dotenv->loadEnv();
+        $this->assertSame(false, getenv('VAR'));
+    }
+
+    public function testNotOverriding()
+    {
+        $php_version = getenv('PHP_VERSION');
+        $dotenv = new Dotenv(__DIR__ . '/fixtures/3');
+        $dotenv->loadEnv();
+        $this->assertSame($php_version, $_ENV['PHP_VERSION']);
+    }
+
+    public function testInvalidInt()
+    {
+        $this->expectException(InvalidParameter::class);
+        $dotenv = new Dotenv(__DIR__ . '/fixtures/invalid', '.env.dist', '.error.int');
+        $dotenv->loadEnv();
+
+    }
+
+    public function testInvalidFloat()
+    {
+        $this->expectException(InvalidParameter::class);
+        $dotenv = new Dotenv(__DIR__ . '/fixtures/invalid', '.env.dist', '.error.float');
+        $dotenv->loadEnv();
+
+    }
+
 }
