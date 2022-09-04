88566c34c38b6ee4f11b2f25d98c88be0c2f5850
enjoys <deadly.info@gmail.com>
2022-09-04 19:52:10 +0300

    implicitly converting an object with a __toString method to a string, also implicitly converting an NULL to empty string

--- src/Parser/Lines/EnvLine.php
+++ src/Parser/Lines/EnvLine.php
@@ -19,7 +19,8 @@ final class EnvLine implements LineInterface
     public function __toString(): string
     {
         $format = ($this->value === null) ? '%s%s%s' : '%s=%s%s';
-        return sprintf($format, (string)$this->key, (string)$this->value, (string)$this->comment);
+        /** @psalm-suppress  ImplicitToStringCast, PossiblyNullArgument */
+        return sprintf($format, $this->key, $this->value, $this->comment);
     }
 
     public function getKey(): Key
