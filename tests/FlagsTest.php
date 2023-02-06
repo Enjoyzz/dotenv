f96a41676ef9dafa8bee8a6f3fd95ba96afd41f1
enjoys <deadly.info@gmail.com>
2023-02-06 12:12:37 +0300

    replace deprecated method
    (cherry picked from commit f7e9139ddb2676f465c4ff5ac04bd59c2f0cc8d1)

--- tests/FlagsTest.php
+++ tests/FlagsTest.php
@@ -26,22 +26,23 @@ final class FlagsTest extends TestCase
 
     public function testFlag_CLEAR_MEMORY_AFTER_LOAD_ENV()
     {
-        $this->expectErrorMessage('Typed property Enjoys\Dotenv\Dotenv::$envCollection must not be accessed before initialization');
-        $dotenv = new Dotenv(__DIR__.'/fixtures/2/.env', null, null,  Dotenv::CLEAR_MEMORY_AFTER_LOAD_ENV);
+
+        $this->expectExceptionMessage('Typed property Enjoys\Dotenv\Dotenv::$envCollection must not be accessed before initialization');
+        $dotenv = new Dotenv(__DIR__.'/fixtures/2/.env', null, null, Dotenv::CLEAR_MEMORY_AFTER_LOAD_ENV);
         $dotenv->loadEnv();
         $result = $dotenv->getEnvCollection();
     }
 
     public function testFlag_POPULATE_SERVER()
     {
-        $dotenv = new Dotenv(__DIR__.'/fixtures/2/.env', null, null,  Dotenv::POPULATE_SERVER|Dotenv::CAST_TYPE_ENV_VALUE);
+        $dotenv = new Dotenv(__DIR__.'/fixtures/2/.env', null, null, Dotenv::POPULATE_SERVER|Dotenv::CAST_TYPE_ENV_VALUE);
         $dotenv->loadEnv();
         $this->assertSame(42, $_SERVER['VAR_1_3']);
     }
 
     public function testFlag_POPULATE_PUTENV()
     {
-        $dotenv = new Dotenv(__DIR__.'/fixtures/2/.env', null, null,  Dotenv::POPULATE_PUTENV|Dotenv::CAST_TYPE_ENV_VALUE);
+        $dotenv = new Dotenv(__DIR__.'/fixtures/2/.env', null, null, Dotenv::POPULATE_PUTENV|Dotenv::CAST_TYPE_ENV_VALUE);
         $dotenv->loadEnv();
         $this->assertSame('42', getenv('VAR_1_3'));
     }
